2333e532d46ae2dc8261bcc16a5baec3d88ab4f7
Marc Anthony Tumminello <marcanthonytumminello@mail.adelphi.edu>
2025-05-03 16:43:23 -0400

    ready to go"

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,11 @@ function App() {
 
   return (
     <>
-      <div>hello world</div>
+      <main className='h-screen w-full bg-neutral-700 flex flex-col items-center'>
+        <div className='text-neutral-200 py-5'>
+          <h1 className='text-3xl'>Deli Dash</h1>
+        </div>
+      </main>
     </>
   )
 }
